9977fcd50fee6929755d686ce757343c1f688312
Nagendra Kadumuru <nagendrakadumuru83@gmail.com>
2025-04-22 00:27:30 +0530

    colors added

--- 12-colors.sh
+++ 12-colors.sh
@@ -8,10 +8,10 @@ Y="\e[33m"
 VALIDATE(){
     if [ $1 -ne 0 ]
     then 
-        echo "$2......$R FAILURE"
+        echo -e "$2......$R FAILURE"
         exit 1
     else 
-        echo "$2.......$G SUCCESS"
+        echo -e "$2.......$G SUCCESS"
     fi
 }
 
@@ -28,7 +28,7 @@ then
     dnf install mysql -y
     VALIDATE $? "Installing MySQL"
 else 
-    echo "MySQL is already.......$Y INSTALLED"
+    echo -e "MySQL is already.......$Y INSTALLED"
 fi
 
 dnf list installed git
@@ -38,7 +38,7 @@ then
     dnf install git -y
     VALIDATE $? "Installing Git"
 else 
-    echo "Git is already.......$Y INSTALLED"
+    echo -e "Git is already.......$Y INSTALLED"
 fi
 
 
