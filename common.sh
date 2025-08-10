02f2ed99ac1f501edf68797e87c5e67fd7017a03
Nagendra Kadumuru <nagendrakadumuru83@gmail.com>
2025-08-10 11:29:31 +0530

    common.sh file changes

--- common.sh
+++ common.sh
@@ -8,7 +8,7 @@ N="\e[0m"
 
 LOGS_FOLDER="/var/logs/shellscript-logs"
 LOG_FILE=$(echo $0 | cut -d "." -f1)
-TIMESTAMP=$(date +%Y-%m-%d-%H=%M-%S)
+TIMESTAMP=$(date +%Y-%m-%d-%H-%M-%S)
 LOG_FILE_NAME="$LOGS_FOLDER/$LOG_FILE-$TIMESTAMP"
 
 VALIDATE(){
